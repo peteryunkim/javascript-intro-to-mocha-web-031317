9e11e3f09d00f59445c89e8c05f62b4cfe65d859
github username <github email address>
2017-05-01 21:50:58 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(){
+  return "I love mint chocolate chip"
+}
+
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+ return `Hey, ${name}!`
+}
+
+function shouting(string) {
+ return string.toUpperCase()
+}
+
+function roundDown(num) {
+ return Math.floor(num)
+}
